905faa9ba042b41c4d7ec9f9fdc52f844ba4bf9f
shiyu <971586331@qq.com>
2026-09-01 14:52:25 +0800

    添加fifo例子

---
+++ ip_fifo/ip_fifo.srcs/sources_1/new/fifo_rd.v
@@ -0,0 +1,50 @@
+//****************************************************************************************//
+
+module fifo_rd(
+    //system clock
+    input               rd_clk      , //时钟信号
+    input               rst_n       , //复位信号
+    //FIFO interface
+    input               rd_rst_busy , //读复位忙信号
+    input        [7:0]  fifo_rd_data, //从FIFO读出的数据
+    input               full        , //FIFO满信号
+    input               almost_empty, //FIFO将空信号
+    output  reg         fifo_rd_en    //FIFO读使能
+);
+
+//reg define
+reg       full_d0;
+reg       full_d1;
+
+//*****************************************************
+//**                    main code
+//*****************************************************
+
+//因为full信号是属于FIFO写时钟域的
+//所以对full打两拍同步到读时钟域下
+always @(posedge rd_clk or negedge rst_n) begin
+    if(!rst_n) begin
+        full_d0 <= 1'b0;
+        full_d1 <= 1'b0;
+    end
+    else begin
+        full_d0 <= full;
+        full_d1 <= full_d0;
+    end
+end    
+    
+//对fifo_rd_en进行赋值,FIFO写满之后开始读，读空之后停止读
+always @(posedge rd_clk or negedge rst_n) begin
+    if(!rst_n) 
+        fifo_rd_en <= 1'b0;
+    else if(!rd_rst_busy) begin
+        if(full_d1)
+           fifo_rd_en <= 1'b1;
+        else if(almost_empty)
+           fifo_rd_en <= 1'b0; 
+    end
+    else
+        fifo_rd_en <= 1'b0;
+end
+
+endmodule
